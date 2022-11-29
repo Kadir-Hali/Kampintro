9c1d90804683c1e3757d3752554254eb859c6ef0
Kadir Hali <kadirhali07@gmail.com>
2022-11-30 00:13:01 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kurs kurs1= new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Engin Demiroğ";
+            kurs1.IzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Python";
+            kurs2.Egitmen = "Kadir Halı";
+            kurs2.IzlenmeOrani = 80;
+
+            Console.WriteLine(kurs1.KursAdi+" : " + kurs1.Egitmen);
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOrani { get; set; }
+    }
+}
