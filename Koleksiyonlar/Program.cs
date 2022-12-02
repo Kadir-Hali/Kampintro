de64309b307b3e6c7b7d4b8a5c7af8ebcdb2ea41
Kadir Hali <kadirhali07@gmail.com>
2022-12-03 01:36:34 +0300

    3.12.2022

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//string[] isimler = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+//Console.WriteLine(isimler[0]);
+//Console.WriteLine(isimler[1]);
+//Console.WriteLine(isimler[2]);
+//Console.WriteLine(isimler[3]);
+
+//isimler=new string[5];
+//isimler[4] = "İlker";
+//Console.WriteLine(isimler[4]);
+//Console.WriteLine(isimler[0]); //boş geldi.
+
+List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+Console.WriteLine(isimler2[0]);
+Console.WriteLine(isimler2[1]);
+Console.WriteLine(isimler2[2]);
+Console.WriteLine(isimler2[3]);
+isimler2.Add("İlker");
+Console.WriteLine(isimler2[4]);
+Console.WriteLine(isimler2[0]);
