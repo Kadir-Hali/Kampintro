72a90a99feac4ab9a097aab4ca5df8a657f3900b
Kadir Hali <kadirhali07@gmail.com>
2022-12-03 22:36:00 +0300

    3.12.2022

--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using GenericsIntro;
+
+MyList<string> isimler = new MyList<string>();
+isimler.Add("Kadir");
+Console.WriteLine(isimler.Length);
+
+isimler.Add("Berat");
+Console.WriteLine(isimler.Length);
+
+foreach (var isim in isimler.Items)
+{
+    Console.WriteLine(isim);
+}
