f556727a6a587f436e56b2b0654ca43d36be465c
Kadir Hali <kadirhali07@gmail.com>
2022-12-05 21:04:50 +0300

    5.12.2022

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyDictionary;
+
+myDictionary<int, string> ogrenci = new myDictionary<int, string>();
+ogrenci.Add(1, "Kadir");
+ogrenci.Add(2, "Berat");
+ogrenci.Add(3, "Doğa");
+ogrenci.Add(4, "Taha");
+
+Console.WriteLine(ogrenci.Length);
---
+++ MyDictionary/myDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class myDictionary<key, value>
+    {
+        key[] k;
+        value[] v;
+
+        public myDictionary()
+        {
+            k = new key[0];
+            v = new value[0];
+        }
+        public void Add(key key, value value)
+        {
+            key[] ktemparray = k;
+            k = new key[k.Length + 1];
+            value[] vtemparray = v;
+            v = new value[v.Length + 1];
+            for (int i = 0; i < ktemparray.Length; i++)
+            {
+                k[i] = ktemparray[i];
+                v[i] = vtemparray[i];
+            }
+            v[v.Length - 1] = value;
+            k[k.Length - 1] = key;
+        }
+        public int Length
+        {
+            get { return v.Length; }
+
+        }
+    }
+
+}
