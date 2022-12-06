58511940b1e008e51d0c1c0c7d0cc3f79216dec0
Kadir Hali <kadirhali07@gmail.com>
2022-12-06 23:59:14 +0300

    6.12.2022

---
+++ OOP3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OOP3;
+
+IKrediManager ihtiyacKrediManager=new IhtiyacKrediManager();
+ihtiyacKrediManager.Hesapla();
+
+IKrediManager konutKrediManager = new KonutKrediManager();
+konutKrediManager.Hesapla();
+
+IKrediManager tasitKrediManager = new TasitKrediManager();
+tasitKrediManager.Hesapla();
