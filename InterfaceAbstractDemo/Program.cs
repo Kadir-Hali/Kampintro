e926c73a14abad008b211d392b2b4a5e08a5b15a
Kadir Hali <kadirhali07@gmail.com>
2022-12-21 01:47:21 +0300

    21.12.2022

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BaseCustomerManager customerManager = new NeroCustomerManager();
+            customerManager.Save(new Entities.Customer {DateOfBirth = new DateTime(2000,2,10),FirstName="Kadir",LastName="Halı",NationalityId="11111111111" });
+        }
+    }
+}
+
